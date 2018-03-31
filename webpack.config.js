75b549a9b542eb7073e4490786025ac884b1d7c4
brunooojapa <brunooojapa@gmail.com>
2018-03-31 18:37:31 -0300

    :wrench: config webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	entry: './app/App.js',
+	output: {
+		path: './public',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		inline: true,
+		contentBase: './public',
+		port: 8080
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
+};
