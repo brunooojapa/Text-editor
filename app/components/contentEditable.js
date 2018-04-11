c8b9dc6a1f7069e74bc69f37c5fc8c1c8bd88df9
brunooojapa <brunooojapa@gmail.com>
2018-04-11 02:53:51 -0300

    :construction:  update content

--- app/components/contentEditable.js
+++ app/components/contentEditable.js
@@ -3,19 +3,22 @@ import React from 'react';
 const textEdit = () => (
 	<div>
 		<p id="pp" style={textStyle} contentEditable="true">
-			text
+			lorem20khoihio iohoi iuoi oi io iouoi
 		</p>
 	</div>
 );
 onkeydown = e => {
 	if (e.ctrlKey && e.keyCode == 'B'.charCodeAt(0)) {
 		e.preventDefault();
-		let text = document.getElementById('pp').copy;
+		let text = document.getElementById('pp').innerHTML;
 		let bold = window.getSelection().toString();
 		//Todo:
-		document.getElementById('pp').innerHTML = bold.bold();
-		console.log(bold.bold());
+		console.log(bold);
+
+		let result = text.replace(bold, bold.bold());
+		document.getElementById('pp').innerHTML = result.toString();
 	}
+
 	if (e.ctrlKey && e.keyCode == 'I'.charCodeAt(0)) {
 		e.preventDefault();
 		let ita = window.getSelection().toString();
@@ -28,6 +31,13 @@ onkeydown = e => {
 
 	if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
 		e.preventDefault();
+		let text = document.getElementById('pp').copy;
+		let underlined = window.getSelection().toString();
+		var x = document.createElement('U');
+		var t = document.createTextNode(underlined);
+		x.appendChild(t);
+		document.getElementById('pp').innerHTML = '';
+		document.getElementById('pp').appendChild(x);
 	}
 };
 
